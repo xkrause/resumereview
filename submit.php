96c1f131e5b45b7e32eb1df654a8d6c3bc162a5a
Chau Duong <chau.duong1995@gmail.com>
2016-05-05 13:51:05 -0700

    Add PHP functions and PHPMailer

--- submit.php
+++ submit.php
@@ -1,10 +1,15 @@
 <?php
+    //This is for PHPMailer
+    require 'PHPMailer/PHPMailerAutoload.php';
+
     $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $file_name = basename($_FILES["fileToUpload"]["name"]). " " . $_POST['uploaderName'] . " " . date("l jS \of F Y h:i:s A");
+    $target_file = $target_dir . $file_name;
+    
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
-    // Check if file already exists
+    // Check if file already exists. Almost would neve happen
     if (file_exists($target_file)) {
         echo "Sorry, file already exists. ";
         $uploadOk = 0;
@@ -24,6 +29,19 @@
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             header("Location: https://review-my-resume.myshopify.com");
             echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            
+               //Sending email
+                $email = new PHPMailer();
+                $email->From      = 'reviewmyresume@hotmail.com';
+                $email->FromName  = 'ReviewMyResume';
+                $email->Subject   = 'Your attached file';
+                $email->Body      = 'This is the resume';
+                $email->AddAddress( 'chau.duong1995@yahoo.com' );
+                
+                $email->AddAttachment( $target_file , $file_name );
+                
+                return $email->Send();
+
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
